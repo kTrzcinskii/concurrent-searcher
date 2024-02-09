830e2060edcfd3c4904b9c6aab0685fd3c5fcc44
kTrzcinskii <kt.trzcinski@gmail.com>
2024-02-09 18:26:57 +0100

    Add option to specify phrase to be looked for

--- src/concurrent_searcher.c
+++ src/concurrent_searcher.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "concurrent_searcher.h"
@@ -9,25 +10,31 @@ int main(int argc, char **argv)
     concurrent_searcher_args_t args;
     read_arguments(argc, argv, &args);
 
-    directory_list_clear(&args.dir_list);
+    clear_arguments(&args);
     return EXIT_SUCCESS;
 }
 
 void usage(char *pname)
 {
-    fprintf(stderr, "USAGE: %s [-r] [-t threads_num] directories\n", pname);
+    fprintf(stderr, "USAGE: %s [-r] [-t threads_num] -p phrase directories\n", pname);
     fprintf(stderr, "r - search directories recursively\n");
     fprintf(stderr, "threads_num - number of threads to be created to concurrently search through directories. Default value is minimum from number of provided directories and max range [integer from range %d-%d]\n", MIN_THREADS_NUM, MAX_THREADS_NUM);
+    fprintf(stderr, "phrase - phrase to be looked for inside every file in provided directories\n");
     fprintf(stderr, "directories - path to directories (separated by spaces) in which files should be checked\n");
     exit(EXIT_FAILURE);
 }
 
+// TODO: add option to get phrase from file
+// TODO: add option to get directories from file
+// TODO: add option to get output to the file
 void read_arguments(int argc, char **argv, concurrent_searcher_args_t *args)
 {
     char c;
     int is_recursive = 0;
     size_t threads_num = 0;
-    while ((c = getopt(argc, argv, "rt:")) != -1)
+    char *p = NULL;
+
+    while ((c = getopt(argc, argv, "rt:p:")) != -1)
     {
         switch (c)
         {
@@ -39,11 +46,20 @@ void read_arguments(int argc, char **argv, concurrent_searcher_args_t *args)
             if (threads_num < MIN_THREADS_NUM || threads_num > MAX_THREADS_NUM)
                 usage(argv[0]);
             break;
+        case 'p':
+            p = malloc(sizeof(char) * (strlen(optarg) + 1));
+            if (!p)
+                ERR("malloc", ALLOCATION_ERROR);
+            strcpy(p, optarg);
+            break;
         case '?':
             usage(argv[0]);
             break;
         }
     }
+
+    if (!p)
+        usage(argv[0]);
 
     int err;
     directories_list_t list = directory_list_init();
@@ -54,4 +70,11 @@ void read_arguments(int argc, char **argv, concurrent_searcher_args_t *args)
     args->dir_list = list;
     args->recursively = is_recursive;
     args->threads_num = threads_num == 0 ? list.count : threads_num;
+    args->phrase = p;
+}
+
+void clear_arguments(concurrent_searcher_args_t *args)
+{
+    directory_list_clear(&args->dir_list);
+    free(args->phrase);
 }
--- src/concurrent_searcher.h
+++ src/concurrent_searcher.h
@@ -11,11 +11,13 @@
 typedef struct concurrent_searcher_args
 {
     directories_list_t dir_list;
+    char *phrase;
     size_t threads_num;
     int recursively;
 } concurrent_searcher_args_t;
 
 void usage(char *pname);
 void read_arguments(int argc, char **argv, concurrent_searcher_args_t *args);
+void clear_arguments(concurrent_searcher_args_t *args);
 
 #endif // CONCURRENT_SEARCHER
