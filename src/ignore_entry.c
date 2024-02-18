6fc5859fdea036260267390a200e21286006f741
kTrzcinskii <kt.trzcinski@gmail.com>
2024-02-18 19:40:48 +0100

    Make `ignore_entry_t` to store `ino_t` instead of file name

--- src/ignore_entry.c
+++ src/ignore_entry.c
@@ -1,4 +1,4 @@
-#include <string.h>
+#include <errno.h>
 
 #include "error_handler.h"
 #include "file_content.h"
@@ -9,67 +9,67 @@ ignore_entry_t ignore_entry_create(char *path)
     ignore_entry_t ignore_entry;
 
     file_content_t content = load_file(path, LOAD_MODE_REMOVE_N);
-    char **entries = malloc(sizeof(char *) * content.lines_num);
+    ino_t *entries = malloc(sizeof(ino_t) * content.lines_num);
     if (!entries)
         ERR("malloc", ALLOCATION_ERROR);
+    struct stat st_buffer;
     for (size_t i = 0; i < content.lines_num; i++)
     {
-        char *s = strdup(content.lines[i]);
-        if (!s)
-            ERR("stdup", GENERAL_ERROR);
-        entries[i] = s;
+        if (stat(content.lines[i], &st_buffer))
+            ERR("stat", GENERAL_ERROR);
+        entries[i] = st_buffer.st_ino;
     }
 
     ignore_entry.entries = entries;
     ignore_entry.count = content.lines_num;
     file_content_clear(content);
 
-    quicksort_string(ignore_entry.entries, 0, ignore_entry.count - 1);
+    quicksort_ino(ignore_entry.entries, 0, ignore_entry.count - 1);
     return ignore_entry;
 }
 
-void quicksort_string(char **array, size_t left, size_t right)
+void quicksort_ino(ino_t *array, size_t left, size_t right)
 {
     while (right > left)
     {
-        size_t j = partition_string(array, left, right);
+        size_t j = partition_ino(array, left, right);
         if (j - left < right - j)
         {
-            quicksort_string(array, left, j - 1);
+            quicksort_ino(array, left, j - 1);
             left = j + 1;
         }
         else
         {
-            quicksort_string(array, j + 1, right);
+            quicksort_ino(array, j + 1, right);
             right = j - 1;
         }
     }
 }
 
-size_t partition_string(char **array, size_t left, size_t right)
+size_t partition_ino(ino_t *array, size_t left, size_t right)
 {
-    char *v = array[right];
+    ino_t v = array[right];
 
     size_t i = left - 1;
     for (size_t j = left; j < right; j++)
     {
-        if (strcmp(array[j], v) <= 0)
+        if (array[j] <= v)
         {
             i++;
             if (i != j)
-                swap_string(&array[i], &array[j]);
+                swap_ino(&array[i], &array[j]);
         }
     }
 
     i++;
-    swap_string(&array[i], &array[right]);
+    swap_ino(&array[i], &array[right]);
 
     return i;
 }
 
-void swap_string(char **s1, char **s2)
+void swap_ino(ino_t *s1, ino_t *s2)
 {
-    char *tmp;
+    ino_t tmp;
     tmp = *s1;
     *s1 = *s2;
     *s2 = tmp;
@@ -77,28 +77,38 @@ void swap_string(char **s1, char **s2)
 
 int ignore_entry_contains(ignore_entry_t ignore_entry, char *name)
 {
+    struct stat st_buffer;
+    if (stat(name, &st_buffer))
+    {
+        // file doesnt exist
+        if (errno == ENOENT)
+            return 0;
+        else
+            ERR("stat", GENERAL_ERROR);
+    }
+
+    ino_t ino = st_buffer.st_ino;
+    fprintf(stderr, "\n[entry] found ino: %ld\n", ino);
+
     size_t left = 0;
     size_t right = ignore_entry.count - 1;
     size_t mid;
     while (left < right)
     {
         mid = (left + right) / 2;
-        int result = strcmp(name, ignore_entry.entries[mid]);
-        if (result == 0)
+        if (ino == ignore_entry.entries[mid])
             return 1;
-        if (result < 0)
+        if (ino < ignore_entry.entries[mid])
             right = mid - 1;
         else
             left = mid + 1;
     }
-    if (strcmp(name, ignore_entry.entries[left]) == 0)
+    if (ino == ignore_entry.entries[left])
         return 1;
     return 0;
 }
 
 void ignore_entry_destroy(ignore_entry_t ignore_entry)
 {
-    for (size_t i = 0; i < ignore_entry.count; i++)
-        free(ignore_entry.entries[i]);
     free(ignore_entry.entries);
 }
--- src/ignore_entry.h
+++ src/ignore_entry.h
@@ -2,17 +2,18 @@
 #define IGNORE_ENTRY
 
 #include <stdlib.h>
+#include <sys/stat.h>
 
 typedef struct ignore_entry
 {
-    char **entries;
+    ino_t *entries;
     size_t count;
 } ignore_entry_t;
 
 ignore_entry_t ignore_entry_create(char *path);
-void quicksort_string(char **array, size_t left, size_t right);
-size_t partition_string(char **array, size_t left, size_t right);
-void swap_string(char **s1, char **s2);
+void quicksort_ino(ino_t *array, size_t left, size_t right);
+size_t partition_ino(ino_t *array, size_t left, size_t right);
+void swap_ino(ino_t *s1, ino_t *s2);
 int ignore_entry_contains(ignore_entry_t ignore_entry, char *name);
 void ignore_entry_destroy(ignore_entry_t ignore_entry);
 
